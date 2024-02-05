3a1f04a833c4d359254bb6ce584a5379ab7d2735
cedraz <icaro.cedraz@ufba.br>
2024-02-05 20:06:21 -0300

    fix api key leak

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,10 +1,11 @@
 import nodemailer from 'nodemailer'
+import { env } from 'process'
 
 export const transporter = nodemailer.createTransport({
   host: 'smtp.sendgrid.net',
   port: 465,
   auth: {
     user: 'apikey',
-    pass: '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'
+    pass: env.API_KEY
   }
 })
